9e05f9d18dced9ac87b90e14266af096f9ce0a26
Romulo Auccapuclla <rauccapuclla@gmail.com>
2019-03-23 11:00:19 -0500

    fix

--- terraform/devops-tools/main.tf
+++ terraform/devops-tools/main.tf
@@ -1,6 +1,6 @@
-resource "aws_security_group" "jenkins-sg" {
-  name = "jenkins-sg-allow"
-  description = "Jenkins Security Group"
+resource "aws_security_group" "allow-ingress-traffic" {
+  name = "allow-ingress"
+  description = "DevOps tools sg"
 
   ingress {
     from_port = 22
@@ -21,55 +21,16 @@ resource "aws_security_group" "jenkins-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_security_group" "sonar-sg" {
-  name = "sonar-sg-allow"
-  description = "Sonar Security Group"
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port = 9000
-    to_port = 9000
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "jenkins" {
   ami           = "ami-0302f3ec240b9d23c"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name = "demo"
-  security_groups = ["${aws_security_group.jenkins-sg.name}"]
+  security_groups = ["${aws_security_group.allow-ingress-traffic.name}"]
   user_data = <<EOF
   #!/bin/bash
   docker run -d -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock --name jenkins rauccapuclla/jenkins-docker:latest
 EOF
   tags = {
     Name = "jenkins-node"
-  }
-}
-
-resource "aws_instance" "sonar" {
-  ami           = "ami-0302f3ec240b9d23c"
-  instance_type = "t2.micro"
-  key_name = "demo"
-  security_groups = ["${aws_security_group.sonar-sg.name}"]
-  user_data = <<EOF
-  #!/bin/bash
-  docker run -d -p 9000:9000 --name sonar sonarqube:lts
-EOF
-  tags = {
-    Name = "sonar-node"
   }
 }
--- terraform/devops-tools/output.tf
+++ terraform/devops-tools/output.tf
@@ -2,6 +2,3 @@ output "jenkins_public_ip" {
   value = "${aws_instance.jenkins.public_ip}"
 }
 
-output "sonar_public_ip" {
-  value = "${aws_instance.sonar.public_ip}"
-}
